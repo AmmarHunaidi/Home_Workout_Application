7999e76ac30f638036e1c4c347db1db06c514d41
Philip <philipp565dro@gmail.com>
2022-05-17 20:14:56 +0300

    added ar local files in lang, CheckLang middleware & a lang test route in api.php

---
+++ lang/ar/messages.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | App messages Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during App for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+    'somthing went wrong' => 'حدث خطأ ما :(',
+];
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\App;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['lang']], function () {
+    // Route::get('/testlang', function () {
+    //     return response()->json(['message' => __('messages.somthing went wrong'), 'local' => App::currentLocale()], 200);
+    // });
 });
