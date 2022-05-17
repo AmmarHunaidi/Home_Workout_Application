931bc21e7dcc3492e6eea21ff27e6a03560fd115
Philip <philipp565dro@gmail.com>
2022-05-17 23:53:03 +0300

    added Emails intrfaces in ar & in ,EmailTrait

---
+++ app/Traits/EmailTrait.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Http\Request;
+
+trait EmailTrait
+{
+}
+// use App\Traits\EmailTrait; befor the controller class
+// use EmailTrait; inside the controller class
