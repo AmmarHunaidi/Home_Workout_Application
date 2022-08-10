2078986633f93a4204ce49f293d04208d4003762
ammar-hunaidi-proceedit <ammar.hunaidi.01@gmail.com>
2022-08-10 10:57:13 +0300

    Exception Handling

--- app/Http/Controllers/ExcersiseController.php
+++ app/Http/Controllers/ExcersiseController.php
@@ -9,6 +9,7 @@ use App\Models\ExcersiseMedia;
 use App\Models\User;
 use App\Traits\GeneralTrait;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -22,89 +23,50 @@ class ExcersiseController extends Controller
     use GeneralTrait;
     function index()
     {
-        $result = [];
-        $excersises = Excersise::all(['id', 'name', 'excersise_media_url', 'user_id','description']);
-        foreach ($excersises as $excersise) {
-            $excersise->user_id = User::find($excersise->user_id)->only(['id', 'f_name', 'l_name', 'prof_img_url']);
-            $excersise->excersise_media_url = 'storage/images/excersise/' . $excersise->excersise_media_url;
-            $result[] = $excersise;
+        try {
+            $result = [];
+            $excersises = Excersise::all(['id', 'name', 'excersise_media_url', 'user_id', 'description']);
+            foreach ($excersises as $excersise) {
+                $excersise->user_id = User::find($excersise->user_id)->only(['id', 'f_name', 'l_name', 'prof_img_url']);
+                $excersise->excersise_media_url = 'storage/images/excersise/' . $excersise->excersise_media_url;
+                $result[] = $excersise;
+            }
+            return $this->success("Success", $result, 200);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
-        return $this->success("Success", $result, 200);
     }
 
     public function show($id)
     {
-        return $this->success("Excersise", Excersise::find($id), 200);
+        try {
+            return $this->success("Excersise", Excersise::find($id), 200);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
+        }
     }
 
     public function create(Request $request)
     {
-        if (in_array($request->user()->role_id, [2, 4, 5])) {
-            $fields = Validator::make($request->only(['name', 'burn_calories', 'excersise_media', 'description']), [
-                'name' => 'required|string',
-                'burn_calories' => 'required|string',
-                'description' => 'required|string',
-                'excersise_media' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg,bmp'
-            ]);
-            if ($fields->fails()) {
-                return $this->fail($fields->errors()->first(), 400);
-            }
-            $fields = $fields->safe()->all();
-            if (Excersise::where('name', $fields['name'])->exists()) {
-                return $this->fail('Name already taken!', 400);
-            }
-            $fields['burn_calories'] = (int) $fields['burn_calories'];
-            $fields['user_id'] = $request->user()->id;
-            $excersise = Excersise::create($fields);
-            if ($request->hasFile('excersise_media')) {
-                $destination_path = 'public/images/excersise';
-                $image = $request->file('excersise_media');
-                $randomString = Str::random(30);
-                $image_name = $excersise->id . '/' . $randomString . $image->getClientOriginalName();
-                $path = $image->storeAs($destination_path, $image_name);
-                $excersise->excersise_media_url = $image_name;
-            }
-            $excersise->created_at = (string)Carbon::parse($excersise->created_at)->utcOffset((int)config('app.timeoffset'))->format('Y/m/d g:i A');
-            $excersise->update();
-            return $this->success("Created Successfully", $excersise, 201);
-        } else {
-            return $this->fail("Permission not allowed!", 400);
-        }
-    }
-
-    public function edit(Request $request, $id)
-    {
-        $user = User::find(Auth::id());
-        if (in_array($user->role_id, [2, 4, 5])) {
-            $fields = Validator::make($request->only(['name', 'burn_calories', 'excersise_media', 'description']), [
-                'name' => 'string|required',
-                'burn_calories' => 'integer|required',
-                'description' => 'required|string',
-                'excersise_media' => 'image|mimes:jpg,png,jpeg,gif,svg,bmp'
-            ]);
-            if ($fields->fails()) {
-                return $this->fail($fields->errors()->first(), 400);
-            }
-            if (!Excersise::where('id', $id)->exists()) {
-                return $this->fail("Not Found", 400);
-            }
-            $excersise = Excersise::find($id);
-            $fields = $fields->safe()->all();;
-            if ($excersise->user->id == $user->id || in_array(User::find(Auth::id())->role_id, [4, 5])) {
-                if (in_array($fields['name'], Excersise::where('name', '!=', $excersise->name)->get('name')->toArray())) {
-                    return $this->fail("Name Already Exists", 400);
+        try {
+            if (in_array($request->user()->role_id, [2, 4, 5])) {
+                $fields = Validator::make($request->only(['name', 'burn_calories', 'excersise_media', 'description']), [
+                    'name' => 'required|string',
+                    'burn_calories' => 'required|string',
+                    'description' => 'required|string',
+                    'excersise_media' => 'nullable|image|mimes:jpg,png,jpeg,gif,svg,bmp'
+                ]);
+                if ($fields->fails()) {
+                    return $this->fail($fields->errors()->first(), 400);
                 }
-                if ($fields['name'] != $excersise->name)
-                    $excersise->name = $fields['name'];
-                if ($fields['burn_calories'] != $excersise->burn_calories)
-                    $excersise->burn_calories = $fields['burn_calories'];
-                if ($fields['description'] != $excersise->description)
-                    $excersise->description = $fields['description'];
+                $fields = $fields->safe()->all();
+                if (Excersise::where('name', $fields['name'])->exists()) {
+                    return $this->fail('Name already taken!', 400);
+                }
+                $fields['burn_calories'] = (int) $fields['burn_calories'];
+                $fields['user_id'] = $request->user()->id;
+                $excersise = Excersise::create($fields);
                 if ($request->hasFile('excersise_media')) {
-                    if($excersise->excersise_media_url != "Default/1.jpg")
-                    {
-                        Storage::delete('public/images/excersise/' . $excersise->excersise_media_url);
-                    }
                     $destination_path = 'public/images/excersise';
                     $image = $request->file('excersise_media');
                     $randomString = Str::random(30);
@@ -112,23 +74,81 @@ class ExcersiseController extends Controller
                     $path = $image->storeAs($destination_path, $image_name);
                     $excersise->excersise_media_url = $image_name;
                 }
-                $excersise->updated_at = (string)Carbon::parse($excersise->created_at)->utcOffset((int)config('app.timeoffset'))->format('Y/m/d g:i A');
+                $excersise->created_at = (string)Carbon::parse($excersise->created_at)->utcOffset((int)config('app.timeoffset'))->format('Y/m/d g:i A');
                 $excersise->update();
-                return $this->success("Successfully", $excersise, 200);
+                return $this->success("Created Successfully", $excersise, 201);
+            } else {
+                return $this->fail("Permission not allowed!", 400);
             }
-            return $this->fail("Failed", 400);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
+        }
+    }
+
+    public function edit(Request $request, $id)
+    {
+        try {
+            $user = User::find(Auth::id());
+            if (in_array($user->role_id, [2, 4, 5])) {
+                $fields = Validator::make($request->only(['name', 'burn_calories', 'excersise_media', 'description']), [
+                    'name' => 'string|required',
+                    'burn_calories' => 'integer|required',
+                    'description' => 'required|string',
+                    'excersise_media' => 'image|mimes:jpg,png,jpeg,gif,svg,bmp'
+                ]);
+                if ($fields->fails()) {
+                    return $this->fail($fields->errors()->first(), 400);
+                }
+                if (!Excersise::where('id', $id)->exists()) {
+                    return $this->fail("Not Found", 400);
+                }
+                $excersise = Excersise::find($id);
+                $fields = $fields->safe()->all();;
+                if ($excersise->user->id == $user->id || in_array(User::find(Auth::id())->role_id, [4, 5])) {
+                    if (in_array($fields['name'], Excersise::where('name', '!=', $excersise->name)->get('name')->toArray())) {
+                        return $this->fail("Name Already Exists", 400);
+                    }
+                    if ($fields['name'] != $excersise->name)
+                        $excersise->name = $fields['name'];
+                    if ($fields['burn_calories'] != $excersise->burn_calories)
+                        $excersise->burn_calories = $fields['burn_calories'];
+                    if ($fields['description'] != $excersise->description)
+                        $excersise->description = $fields['description'];
+                    if ($request->hasFile('excersise_media')) {
+                        if ($excersise->excersise_media_url != "Default/1.jpg") {
+                            Storage::delete('public/images/excersise/' . $excersise->excersise_media_url);
+                        }
+                        $destination_path = 'public/images/excersise';
+                        $image = $request->file('excersise_media');
+                        $randomString = Str::random(30);
+                        $image_name = $excersise->id . '/' . $randomString . $image->getClientOriginalName();
+                        $path = $image->storeAs($destination_path, $image_name);
+                        $excersise->excersise_media_url = $image_name;
+                    }
+                    $excersise->updated_at = (string)Carbon::parse($excersise->created_at)->utcOffset((int)config('app.timeoffset'))->format('Y/m/d g:i A');
+                    $excersise->update();
+                    return $this->success("Successfully", $excersise, 200);
+                }
+                return $this->fail("Failed", 400);
+            }
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
     }
 
     public function destroy($id)
     {
-        $user = User::find(Auth::id());
-        $excersise = Excersise::find($id);
-        if (in_array($user->role_id, [4, 5]) || $excersise->user_id == Auth::id()) {
-            Storage::delete('public/images/excersise/' . $excersise->excersise_media_url);
-            $excersise->delete();
-            return $this->success("Success", [], 200);
+        try {
+            $user = User::find(Auth::id());
+            $excersise = Excersise::find($id);
+            if (in_array($user->role_id, [4, 5]) || $excersise->user_id == Auth::id()) {
+                Storage::delete('public/images/excersise/' . $excersise->excersise_media_url);
+                $excersise->delete();
+                return $this->success("Success", [], 200);
+            }
+            return $this->fail("Permission not allowed!", 400);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
-        return $this->fail("Permission not allowed!", 400);
     }
 }
--- app/Http/Controllers/MealController.php
+++ app/Http/Controllers/MealController.php
@@ -5,9 +5,12 @@ namespace App\Http\Controllers;
 use App\Models\Meal;
 use App\Http\Requests\StoreMealRequest;
 use App\Http\Requests\UpdateMealRequest;
+use App\Models\DietMeal;
 use App\Models\Food;
 use App\Models\MealFood;
+use App\Models\User;
 use App\Traits\GeneralTrait;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
@@ -18,67 +21,69 @@ class MealController extends Controller
 
     public function index()
     {
-        $result = [];
-        $meals = Meal::all(['id','type','description','calorie_count']);
-        foreach($meals as $meal)
-        {
-            $food = [];
-            $mealfood = MealFood::where('meal_id',$meal['id'])->get();
-            foreach($mealfood as $mf)
-            {
-                $fd = Food::find($mf->food_id);
-                $fd->food_image_url = 'storage/images/food/' . $fd->food_image_url;
-                if($fd->approval == 1) $food[] = $fd;
+        try {
+            $result = [];
+            $meals = Meal::all(['id', 'type', 'description', 'calorie_count']);
+            foreach ($meals as $meal) {
+                $food = [];
+                $mealfood = MealFood::where('meal_id', $meal['id'])->get();
+                foreach ($mealfood as $mf) {
+                    $fd = Food::find($mf->food_id);
+                    $fd->food_image_url = 'storage/images/food/' . $fd->food_image_url;
+                    if ($fd->approval == 1) $food[] = $fd;
+                }
+                $meal['food_list'] = $food;
+                $result[] = $meal;
             }
-            $meal['food_list'] = $food;
-            $result[] = $meal;
+            return $this->success("Success", $result, 200);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
-        return $this->success("Success" , $result , 200);
     }
 
     public function create(Request $request)
     {
-        if($request->user()->role_id == 3 || $request->user()->role_id == 4 || $request->user()->role_id == 5)
-        {
-            $fields = Validator::make($request->only(['type','description','food_ids','day']), [
-                'type' => 'required|string|in:Breakfast,Dinner,Snack,Lunch',
-                'description' => 'required|string',
-                'food_ids' => 'string|required'
-            ]);
-            if($fields->fails())
-            {
-                return $this->fail($fields->errors()->first(),401);
-            }
-            $fields = $fields->safe()->all();
-            $fields['user_id'] = $request->user()->id;
-            $meal = Meal::create($fields);
-            $food_result = [];
-            $food_list = json_decode($fields['food_ids']);
-            foreach($food_list as $food_id)
-            {
-                $food = Food::find($food_id);
-                $food_result[] = $food;
-                $data = [
-                    'meal_id' => $meal->id,
-                    'food_id' => $food->id,
-                    'user_id' => $request->user()->id
-                ];
-                MealFood::create($data);
-                $meal->calorie_count += $food->calories;
-            }
-            if($request->user()->role_id == 4 || $request->user()->role_id == 5)
-            {
-                $meal->approval = 1;
+        try {
+            if ($request->user()->role_id == 3 || $request->user()->role_id == 4 || $request->user()->role_id == 5) {
+                $fields = Validator::make($request->only(['type', 'description', 'food_ids', 'day']), [
+                    'type' => 'required|string|in:Breakfast,Dinner,Snack,Lunch',
+                    'description' => 'required|string',
+                    'food_ids' => 'string|required'
+                ]);
+                if ($fields->fails()) {
+                    return $this->fail($fields->errors()->first(), 401);
+                }
+                $fields = $fields->safe()->all();
+                $fields['user_id'] = $request->user()->id;
+                $meal = Meal::create($fields);
+                $food_result = [];
+                $food_list = json_decode($fields['food_ids']);
+                foreach ($food_list as $food_id) {
+                    $food = Food::find($food_id);
+                    $food_result[] = $food;
+                    $data = [
+                        'meal_id' => $meal->id,
+                        'food_id' => $food->id,
+                        'user_id' => $request->user()->id
+                    ];
+                    MealFood::create($data);
+                    $meal->calorie_count += $food->calories;
+                }
+                if ($request->user()->role_id == 4 || $request->user()->role_id == 5) {
+                    $meal->approval = 1;
+                    $meal->update();
+                    return $this->success(_("Created Successfully"), $meal, 200);
+                }
                 $meal->update();
-                return $this->success(_("Created Successfully"), $meal, 200);
-            }
-            $meal->update();
-            $message = 'Meal Created Successfully . Awaiting Approval';
+                $message = 'Meal Created Successfully . Awaiting Approval';
                 $data = [
                     'meal' => $meal,
                     'food_list' => $food_result
                 ];
-            return $this->success(_($message), $meal, 200);
+                return $this->success(_($message), $meal, 200);
+            }
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
     }
 
@@ -98,41 +103,36 @@ class MealController extends Controller
 
     public function edit(Request $request)
     {
-        if($request->user()->role_id == 4 || $request->user()->role_id == 5)
-        {
-            $fields = Validator::make($request->only('type','meal_id','description' , 'food_ids'),[
-                'type' => 'string|nullable|in:Breakfast,Dinner,Snack,Lunch',
-                'meal_id' => 'required|integer',
-                'food_ids' => 'required|string',
-                'description' => 'required|string'
-            ]);
-            if($fields->fails())
-            {
-                return $this->fail($fields->errors()->first(),400);
-            }
-            $fields = $fields->safe()->all();
-            $meal = Meal::find($fields['meal_id']);
-            if($meal->type != $fields['type'])
-            {
-                $meal->type = $fields['type'];
-            }
-            if($meal->description != $fields['description'])
-            {
-                $meal->description = $fields['description'];
-            }
-            $food_list = json_decode($fields['food_ids']);
-            $pre_food = [];
-            $mealfood = $meal->mealfood;
-            foreach($food_list as $food_id)
-            {
-                $food = Food::find($food_id);
-                $food_result[] = $food;
-                $mf = MealFood::query()
-                        ->where('food_id' , $food->id)
-                        ->where('meal_id',$meal->id)
+        try {
+            if ($request->user()->role_id == 4 || $request->user()->role_id == 5) {
+                $fields = Validator::make($request->only('type', 'meal_id', 'description', 'food_ids'), [
+                    'type' => 'string|nullable|in:Breakfast,Dinner,Snack,Lunch',
+                    'meal_id' => 'required|integer',
+                    'food_ids' => 'required|string',
+                    'description' => 'required|string'
+                ]);
+                if ($fields->fails()) {
+                    return $this->fail($fields->errors()->first(), 400);
+                }
+                $fields = $fields->safe()->all();
+                $meal = Meal::find($fields['meal_id']);
+                if ($meal->type != $fields['type']) {
+                    $meal->type = $fields['type'];
+                }
+                if ($meal->description != $fields['description']) {
+                    $meal->description = $fields['description'];
+                }
+                $food_list = json_decode($fields['food_ids']);
+                $pre_food = [];
+                $mealfood = $meal->mealfood;
+                foreach ($food_list as $food_id) {
+                    $food = Food::find($food_id);
+                    $food_result[] = $food;
+                    $mf = MealFood::query()
+                        ->where('food_id', $food->id)
+                        ->where('meal_id', $meal->id)
                         ->exists();
-                    if($mf == 0)
-                    {
+                    if ($mf == 0) {
                         MealFood::create([
                             'user_id' => Auth::id(),
                             'meal_id' => $meal->id,
@@ -140,21 +140,19 @@ class MealController extends Controller
                         ]);
                         $meal->calorie_count += $food->calories;
                     }
-            }
-            foreach($mealfood as $food)
-            {
-                $pre_food[] = $food->food_id;
-            }
-            $delete_food = array_diff($pre_food , $food_list);
-            foreach($delete_food as $food_id)
-            {
-                $mealfood = MealFood::query()
-                                    ->where('meal_id' , $meal->id)
-                                    ->where('food_id' , $food_id);
-                $mealfood->delete();
-                $food = Food::find($food_id);
-                $meal->calorie_count -= $food->calories;
-            }
+                }
+                foreach ($mealfood as $food) {
+                    $pre_food[] = $food->food_id;
+                }
+                $delete_food = array_diff($pre_food, $food_list);
+                foreach ($delete_food as $food_id) {
+                    $mealfood = MealFood::query()
+                        ->where('meal_id', $meal->id)
+                        ->where('food_id', $food_id);
+                    $mealfood->delete();
+                    $food = Food::find($food_id);
+                    $meal->calorie_count -= $food->calories;
+                }
                 $meal->update();
                 $meal = Meal::find($meal->id);
                 $message = 'Meal Edited Successfully';
@@ -162,37 +160,33 @@ class MealController extends Controller
                     'meal' => $meal,
                     'food_list' => $food_result
                 ];
-                return $this->success(_($message),$data,200);
-        }
-        else
-        {
-            $message = 'Permission Denied. Not the owner';
-            return $this->fail(_($message),400);
+                return $this->success(_($message), $data, 200);
+            } else {
+                $message = 'Permission Denied. Not the owner';
+                return $this->fail(_($message), 400);
+            }
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
     }
 
 
-    public function destroy(Request $request)
+    public function destroy($id)
     {
-        if($request->user()->role_id == 4 || $request->user()->role_id == 5)
-        {
-            $fields = Validator::make($request->only(['meal_id']),[
-                'meal_id' => 'required|integer'
-            ]);
-            if($fields->fails())
-            {
-                return $this->fail($fields->errors()->first(),400);
+        try {
+            $user = User::find(Auth::id());
+            $meal = Meal::find($id);
+            if (DietMeal::where('meal_id', $meal->id)->exists()) {
+                return $this->fail("Can't Delete Meal Due to it being assigned to a/many Diets!", 400);
             }
-            $fields = $fields->safe()->all();
-            $meal = Meal::find($fields['meal_id']);
-            if($request->user()->id == $meal->user_id){
+            if (in_array($user->role_id, [4, 5]) || $user->id == $meal->user_id) {
                 $meal->mealfood()->delete();
                 $meal->delete();
-                $message = 'Meal Deleted Successfully';
-                return $this->success(_($message),$meal,200);
+                return $this->success('Meal Deleted Successfully', $meal, 200);
             }
-            $message = 'Permission Denied. Not the owner';
-            return $this->fail(_($message),400);
+            return $this->fail('Permission Denied. Not the owner', 400);
+        } catch (Exception $exception) {
+            return $this->fail($exception->getMessage(), 500);
         }
     }
 }
